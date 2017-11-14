6a834d232375b55844455a8d80ffc66fcb7b4b79
900Bill <Apvuera@gmail.com>
2017-11-13 18:32:31 -0600

    Add files via upload

--- src/P1_OutputLoopResult.java
+++ src/P1_OutputLoopResult.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*
   	ISYS 320
   	Name(s):
@@ -9,16 +11,19 @@
 public class P1_OutputLoopResult {
 
 	public static void main(String[] args) {
-		printSumOfSquaresTo5();
+		//TODO Auto-Generated method stub
+		System.out.println("Square of is");
+		Scanner sr = new Scanner(System.in);
+		int ns = sr.nextInt(); 
+		P1_OutputLoopResult p1_OutputLoopResult = new P1_OutputLoopResult();
+		System.out.println("Sum of the squares of "+ns+" numbers is "+P1_OutputLoopResult.sumSquare(ns));
+		
 	}
-	
-	public static void printSumOfSquaresTo5() {
-		for( int i = 1; i <= 5; i++ ) {
-			int finalResult = 0;
-			finalResult = finalResult + i * i;
+	public static int sumSquare(int n) {
+		if(n==0)
+			return 0;
+		else
+			return ((n*n)+sumSquare(n-1));
 		}
 		
-		System.out.println( "Final result is: " + finalResult );
 	}
-
-}
